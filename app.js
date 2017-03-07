f9f9b1a98f0343386fda2c3164ef65c42c9764d1
ochochaban <ogel.chaban@gmail.com>
2017-03-08 02:40:35 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,1 @@
+if(prompt('Введите первую строку',0).toLowerCase().split('').sort().toString() == prompt('Введите вторую строку',1).toLowerCase().split('').sort().toString()) {alert('Анаграмма');} else {alert('Не анаграмма');}
