293ec937d51c77112a7c76874fdd948ecc357d1e
ochochaban <ogel.chaban@gmail.com>
2017-03-10 16:31:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-if(prompt('Введите первую строку',0).toLowerCase().split('').sort().toString() == prompt('Введите вторую строку',1).toLowerCase().split('').sort().toString()) {alert('Строки являются анаграммами');} else {alert('Строки не являются анаграммами');}
+if(prompt('Введите первую строку',0).toLowerCase().split('').sort().join() == prompt('Введите вторую строку',1).toLowerCase().split('').sort().join()) {
+  alert('Строки являются анаграммами');
+} else {
+  alert('Строки не являются анаграммами');
+}
