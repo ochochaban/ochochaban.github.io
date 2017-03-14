09de50cd4c07a40eb39657b29317c1781eba50a2
ochochaban <ogel.chaban@gmail.com>
2017-03-15 00:24:20 +0300

    Update 2_2.js

--- hw_2/2_2.js
+++ hw_2/2_2.js
@@ -6,3 +6,16 @@ for (var i = 0; i < a.length; i++) {
 		c.push(a[i]);
 	}
 }
+
+var a = [1, 3, 5, 7, 9];
+var b = [1, 2, 3, 4];
+var c = [];
+var d = [];
+for (var i = 0; i < a.length; i++) {
+	c[a[i]] = true;
+}
+for (i = 0; i < b.length; i++) {
+	if (c[b[i]x]) {
+		d.push(b[i]);
+	}
+}
